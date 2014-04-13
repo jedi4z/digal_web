5cf923255afca08648ba1687784f471759e45688
Jesús Díaz <gbuuss@gmail.com>
2014-04-13 04:48:24 -0300

    se agrega una barra para dispositivos móviles

--- web/bundles/digal/js/scripts.js
+++ web/bundles/digal/js/scripts.js
@@ -1,28 +1,40 @@
 // Form contact
 $(document).ready(function(){
-    $("#formContact").submit(function(event){
-        event.preventDefault();
-        var postData = $(this).serializeArray();
-		var formURL = $(this).attr("action");
-		var boxInf = $("#boxInf");
-		var inputName = $(".inputName");
-		var inputEmail = $(".inputEmail");
-		var textConsult = $(".textConsult");
-		var buttonSend = $("#buttonSend");
-        $.ajax({
-            url: formURL,
-            type: "POST",
-			data : postData,
-			beforeSend: function (){
-				buttonSend.prop('value', 'Enviando...');				
-				buttonSend.prop("disabled",true);
-			},
-            success: function(result) {
-            	boxInf.fadeIn("slow");
-                boxInf.text(""+result);
-                buttonSend.prop("disabled",false);
-                buttonSend.prop('value', 'Enviar');          
-            }
-        });
+  $("#formContact").submit(function(event){
+    event.preventDefault();
+    var postData = $(this).serializeArray();
+  	var formURL = $(this).attr("action");
+  	var boxInf = $("#boxInf");
+  	var inputName = $(".inputName");
+  	var inputEmail = $(".inputEmail");
+  	var textConsult = $(".textConsult");
+  	var buttonSend = $("#buttonSend");
+    $.ajax({
+      url: formURL,
+      type: "POST",
+  		data : postData,
+  		beforeSend: function (){
+  			buttonSend.prop('value', 'Enviando...');				
+  			buttonSend.prop("disabled",true);
+  		},
+      success: function(result) {
+      	boxInf.fadeIn("slow");
+        boxInf.text(""+result);
+        buttonSend.prop("disabled",false);
+        buttonSend.prop('value', 'Enviar');          
+      }
     });
+  });
+});
+
+// Menu mobile
+$(document).ready(function(){
+  $('#mobile-menu').click(function(event){
+    event.preventDefault();
+    if ($('#wrapper-navbar').is(':hidden')){
+      $('#wrapper-navbar').slideDown(200);
+    }else {
+       $('#wrapper-navbar').slideUp(200);
+    }
+  });
 });
